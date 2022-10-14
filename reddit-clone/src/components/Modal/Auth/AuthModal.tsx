9789b563bc74fe7c1a5715200fa098ae8db6d17b
arbreshazeqiri <arbresha.zeqiri@student.uni-pr.edu>
2022-10-14 11:37:39 +0200

    Login and Sign Up modal forms set

--- reddit-clone/src/components/Modal/Auth/AuthModal.tsx
+++ reddit-clone/src/components/Modal/Auth/AuthModal.tsx
@@ -1,35 +1,58 @@
 import {
-  Button,
+  Text,
+  Flex,
   Modal,
   ModalBody,
   ModalCloseButton,
   ModalContent,
-  ModalFooter,
   ModalHeader,
   ModalOverlay,
-  useDisclosure,
 } from "@chakra-ui/react";
 import React from "react";
+import { useRecoilState } from "recoil";
+import { authModalState } from "../../../atoms/authModalAtom";
+import AuthInputs from "./AuthInputs";
+import OAuthButtons from "./OAuthButton";
 
 const AuthModal: React.FC = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [modalState, setModalState] = useRecoilState(authModalState);
+  const handleClose = () => {
+    setModalState((prev) => ({
+      ...prev,
+      open: false,
+    }));
+  };
   return (
     <>
-      <Button onClick={onOpen}>Open Modal</Button>
-
-      <Modal isOpen={isOpen} onClose={onClose}>
+      <Modal isOpen={modalState.open} onClose={handleClose}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
+          <ModalHeader textAlign="center">
+            {modalState.view === "login" && "Login"}
+            {modalState.view === "signup" && "Sign up"}
+            {modalState.view === "resetPassword" && "Reset Password"}
+          </ModalHeader>
           <ModalCloseButton />
-          <ModalBody>Arbresha Zeqiri ma e mira n bote</ModalBody>
-
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
-              Close
-            </Button>
-            <Button variant="ghost">Secondary Action</Button>
-          </ModalFooter>
+          <ModalBody
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+            pb={6}
+          >
+            <Flex
+              direction="column"
+              align="center"
+              justify="center"
+              width="70%"
+              border="1px solid red"
+            >
+              <OAuthButtons />
+              <Text color="gray.00" fontWeight={700}>OR</Text>
+              <AuthInputs />
+              {/*<ResetPassword /> */}
+            </Flex>
+          </ModalBody>
         </ModalContent>
       </Modal>
     </>
---
+++ reddit-clone/src/components/Modal/Auth/OAuthButton.tsx
@@ -0,0 +1,18 @@
+import { Button, Flex, Image } from "@chakra-ui/react";
+import React from "react";
+
+type OAuthButtonsProps = {};
+
+const OAuthButtons: React.FC<OAuthButtonsProps> = () => {
+  return (
+    <Flex direction="column" width="100%" mb={4}>
+      <Button variant="oauth">
+        <Image src="/images/googlelogo.png" height="20px" mr={4}/>
+        Continue with Google?
+      </Button>
+      <Button>Some Other Provider</Button>
+    </Flex>
+  );
+};
+
+export default OAuthButtons;
