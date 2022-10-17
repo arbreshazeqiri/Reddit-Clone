02eb2338d4a67921f956389f8ff0f5fa86ee5780
arbreshazeqiri <arbresha.zeqiri@student.uni-pr.edu>
2022-10-17 11:55:09 +0200

    Responsive search input set

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,24 +2,30 @@ import { Flex, Image } from "@chakra-ui/react";
 import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../firebase/clientApp";
+import Directory from "./Directory/Directory";
 import RightContent from "./RightContent/RightContent";
 import SearchInput from "./SearchInput";
 
 const Navbar: React.FC = () => {
   const [user, loading, error] = useAuthState(auth);
   return (
-    <Flex bg="white" height="44px" padding="6px 12px">
-      <Flex align="center">
+    <Flex
+      bg="white"
+      height="44px"
+      padding="6px 12px"
+      justifyContent={{ md: "space-between" }}
+    >
+      <Flex align="center" width={{base: "40px", md: "auto"}} mr={{ base: "0", md: 2}}>
         <Image src="/images/redditFace.svg" height="30px" />
         <Image
           src="/images/redditText.svg"
           height="46px"
-          display={{ /*base: "none",*/ md: "unset" }}
+          display={{ base: "none", md: "unset" }}
         />
       </Flex>
-      <SearchInput />
-      {/* <Directory /> */}
-      <RightContent user={user}/>
+      <SearchInput user={user} />
+      {user && <Directory />}
+      <RightContent user={user} />
     </Flex>
   );
 };
