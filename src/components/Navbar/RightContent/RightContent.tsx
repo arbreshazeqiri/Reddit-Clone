ecdebc1e40cfcffd11903c7ed8d1076b14429e32
arbreshazeqiri <arbresha.zeqiri@student.uni-pr.edu>
2022-10-17 10:58:51 +0200

    Navbar icons set upon login

--- src/components/Navbar/RightContent/RightContent.tsx
+++ src/components/Navbar/RightContent/RightContent.tsx
@@ -1,12 +1,13 @@
 import { Button, Flex } from "@chakra-ui/react";
-import { signOut } from "firebase/auth";
+import { signOut, User } from "firebase/auth";
 import React from "react";
 import { auth } from "../../../firebase/clientApp";
 import AuthModal from "../../Modal/Auth/AuthModal";
 import AuthButtons from "./AuthButtons";
+import Icons from "./Icons";
 
 type RightContentProps = {
-  user: any;
+  user?: User | null; //user null or undefined
 };
 
 const RightContent: React.FC<RightContentProps> = ({ user }) => {
@@ -14,11 +15,7 @@ const RightContent: React.FC<RightContentProps> = ({ user }) => {
     <>
       <AuthModal />
       <Flex justify="center" align="center">
-        {user ? (
-          <Button onClick={() => signOut(auth)}>Logout</Button>
-        ) : (
-          <AuthButtons />
-        )}
+        {user ? <Icons /> : <AuthButtons />}
       </Flex>
     </>
   );
